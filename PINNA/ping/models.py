c6e005354880d060953e46617ff7f64dc1f2d41d
Shota Shimazu <hornet.live.mf@gmail.com>
2018-02-13 22:30:03 +0900

    add new model

--- PINNA/ping/models.py
+++ PINNA/ping/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Pings(models.Model):
+  pingid      = models.CharField(max_length = 35, primary_key = True)
+  lat         = models.CharField()
+  lng         = models.CharField()
+  timestamp   = models.CharField()
+  mainstream  = models.CharField()
+  substream   = models.CharField()
