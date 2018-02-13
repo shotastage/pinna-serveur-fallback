b25494e5ab2231c7212c298060b67dccba65a6fc
Shota Shimazu <hornet.live.mf@gmail.com>
2018-02-14 00:01:19 +0900

    add new model

--- PINNA/ping/models.py
+++ PINNA/ping/models.py
@@ -5,8 +5,18 @@ from django.db import models
 
 class Pings(models.Model):
   pingid      = models.CharField(max_length = 35, primary_key = True)
-  lat         = models.CharField()
-  lng         = models.CharField()
-  timestamp   = models.CharField()
-  mainstream  = models.CharField()
-  substream   = models.CharField()
+  lat         = models.CharField(max_length = 35)
+  lng         = models.CharField(max_length = 35)
+  timestamp   = models.CharField(max_length = 35)
+  mainstream  = models.CharField(max_length = 35)
+  substream   = models.CharField(max_length = 35)
+  playtype    = models.CharField(max_length = 35)
+
+
+class PingsSongInfo(models.Model):
+  pingid      = models.CharField(max_length = 35, primary_key = True)
+  title       = models.CharField()
+  artist      = models.CharField()
+  composer    = models.CharField()
+  year        = models.CharField()
+
