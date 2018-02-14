12b98f492d95e0523942bc4ea47d258ca36f5e4f
Shota Shimazu <hornet.live.mf@gmail.com>
2018-02-15 00:06:54 +0900

    remove webpack related issue

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@
 module.exports = {
     entry: "./shell/src/index.tsx",
     output: {
-        filename: "bundle.js",
+        filename: "pinna.bundle.js",
         path: __dirname + "/shell/dist"
     },
     // Enable sourcemaps for debugging webpack's output.
@@ -25,11 +25,23 @@ module.exports = {
     },
     module: {
         rules: [
+            // All files with a '.css' extension will be handled by 'css-loader'
+            { test: /\.css$/, use: 'css-loader' },
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
   
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+            
+            {
+                test: /\.svg$/,
+                use: {
+                    loader: 'svg-url-loader',
+                    options: {
+                        noquotes: true
+                    }
+                }
+            }
         ]
     },  
 };
