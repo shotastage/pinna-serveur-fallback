5a0cf901aa0d34a7e32764a327694f40753af6da
Shota Shimazu <hornet.live.mf@gmail.com>
2018-02-14 13:35:43 +0900

    add copyright

--- PINNA/core/admin.py
+++ PINNA/core/admin.py
@@ -1,3 +1,15 @@
+"""
+PINNA Serveur
+admin.py
+
+Created by Shota Shimazu on 2018/2/14
+
+Copyright (c) 2018 Shota Shimazu All Rights Reserved.
+
+This software is released under the terms of PINNA Software License, see LICENSE for detail.
+https://github.com/shotastage/pinna-music/blob/master/LICENSE
+"""
+
 from django.contrib import admin
 
 # Register your models here.
--- PINNA/core/tests.py
+++ PINNA/core/tests.py
@@ -1,3 +1,15 @@
+"""
+PINNA Serveur
+tests.py
+
+Created by Shota Shimazu on 2018/2/14
+
+Copyright (c) 2018 Shota Shimazu All Rights Reserved.
+
+This software is released under the terms of PINNA Software License, see LICENSE for detail.
+https://github.com/shotastage/pinna-music/blob/master/LICENSE
+"""
+
 from django.test import TestCase
 
 # Create your tests here.
