ca4c33f57f8c2621839f070584b67ee2f0fb6b1c
Shota Shimazu <hornet.live.mf@gmail.com>
2018-02-15 14:47:55 +0900

    fix model issue

--- PINNA/ping/models.py
+++ PINNA/ping/models.py
@@ -5,18 +5,17 @@ from django.db import models
 
 class Pings(models.Model):
   pingid      = models.CharField(max_length = 35, primary_key = True)
-  lat         = models.CharField(max_length = 35)
-  lng         = models.CharField(max_length = 35)
-  timestamp   = models.CharField(max_length = 35)
-  mainstream  = models.CharField(max_length = 35)
-  substream   = models.CharField(max_length = 35)
-  playtype    = models.CharField(max_length = 35)
+  lat         = models.CharField(max_length = 255)
+  lng         = models.CharField(max_length = 255)
+  timestamp   = models.CharField(max_length = 255)
+  mainstream  = models.CharField(max_length = 255)
+  substream   = models.CharField(max_length = 255)
+  playtype    = models.CharField(max_length = 255)
 
 
 class PingsSongInfo(models.Model):
   pingid      = models.CharField(max_length = 35, primary_key = True)
-  title       = models.CharField()
-  artist      = models.CharField()
-  composer    = models.CharField()
-  year        = models.CharField()
-
+  title       = models.CharField(max_length = 255)
+  artist      = models.CharField(max_length = 255)
+  composer    = models.CharField(max_length = 255)
+  year        = models.CharField(max_length = 255)
