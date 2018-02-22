aeef570dbd67ca62730df55c9767c621bdbe7c09
Shota Shimazu <hornet.live.mf@gmail.com>
2018-02-23 00:04:14 +0900

    update ci script

--- PINNA/PINNA/environment/integration.py
+++ PINNA/PINNA/environment/integration.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'PINNA.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pinna_gitlab_ci',
+        'USER': 'gitlab_ci',
+        'PASSWORD': 'test_passwd_for_ci',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
