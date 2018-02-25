0073a1df487ebe1696ae5e11fb96b65c5ca8fc87
Shota Shimazu <hornet.live.mf@gmail.com>
2018-02-25 17:01:08 +0900

    update deps

--- PINNA/PINNA/environment/development.py
+++ PINNA/PINNA/environment/development.py
@@ -14,4 +14,16 @@ import os
 from .common import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
+
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
