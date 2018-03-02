210e60c0a09737ba38784476300bbe57124aec4e
Shota Shimazu <hornet.live.mf@gmail.com>
2018-03-03 02:02:16 +0900

    update model

--- PINNA/ping/models.py
+++ PINNA/ping/models.py
@@ -4,18 +4,36 @@ from django.db import models
 
 
 class Pings(models.Model):
-  pingid      = models.CharField(max_length = 35, primary_key = True)
+  pingid      = models.UUIDField(primary_key = True)
   lat         = models.CharField(max_length = 255)
   lng         = models.CharField(max_length = 255)
-  timestamp   = models.CharField(max_length = 255)
-  mainstream  = models.CharField(max_length = 255)
-  substream   = models.CharField(max_length = 255)
-  playtype    = models.CharField(max_length = 255)
-
-
-class PingsSongInfo(models.Model):
-  pingid      = models.CharField(max_length = 35, primary_key = True)
+  timestamp   = models.DateField()
   title       = models.CharField(max_length = 255)
-  artist      = models.CharField(max_length = 255)
-  composer    = models.CharField(max_length = 255)
-  year        = models.CharField(max_length = 255)
+  description = models.TextField(blank = True)
+  created_by  = models.CharField(max_length = 255)
+  on_deletion = models.BooleanField(default = False)
+  is_private  = models.BooleanField(default = False)
+  pin_type    = models.CharField(max_length = 255)
+  re_pin      = models.IntegerField(default = 0)
+
+
+class PingsSongDetail(Pings):
+  song_id       = models.UUIDField()
+  song_title         = models.CharField(max_length = 255)
+  artist_id     = models.UUIDField()
+  artist        = models.CharField(max_length = 255)
+  genre         = models.CharField(max_length = 255)
+  release_date  = models.DateField()
+  recorded_on   = models.CharField(max_length = 255)
+  favorites     = models.IntegerField()
+  is_hidden     = models.BooleanField()
+
+
+class CompositionDetail(PingsSongDetail):
+  composer_id           = models.UUIDField()
+  composer              = models.CharField(max_length = 255)
+  composer_birthday     = models.DateField()
+  composer_birthplace   = models.CharField(max_length = 255)
+  composer_living_place = models.CharField(max_length = 255)
+  composed_place        = models.CharField(max_length = 255)
+  composed_when         = models.DateTimeField()
