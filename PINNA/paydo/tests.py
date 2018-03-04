917f227a623f022e07a5820119b103a2187c4658
Shota Shimazu <hornet.live.mf@gmail.com>
2018-03-05 00:14:17 +0900

    add copyright

--- PINNA/paydo/tests.py
+++ PINNA/paydo/tests.py
@@ -1,3 +1,15 @@
+"""
+PINNA
+tests.py
+
+Created by Shota Shimazu on 2018/03/05
+
+Copyright (c) 2018 Shota Shimazu All Rights Reserved.
+
+This software is released under the terms of restricted, see LICENSE for detail.
+https://hplab.work/pinna-music/pinna-music/blob/master/LICENSE
+"""
+
 from django.test import TestCase
 
 # Create your tests here.
