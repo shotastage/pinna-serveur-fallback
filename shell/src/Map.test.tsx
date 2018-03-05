32cae1500932789a536b739e00dcffec549474be
Shota Shimazu <hornet.live.mf@gmail.com>
2018-03-05 21:52:19 +0900

    add new React component

---
+++ shell/src/Map.test.tsx
@@ -0,0 +1,20 @@
+/*
+PINNA
+Map.test.tsx
+
+Created by Shota Shimazu on 2018/03/05
+
+Copyright (c) 2018 Shota Shimazu All Rights Reserved.
+
+This software is released under the terms of restricted, see LICENSE for detail.
+https://hplab.work/pinna-music/pinna-music/blob/master/LICENSE
+*/
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './Map';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
