0e190866d2e3c46abb185d2ab6ac4e97892b2a7a
Shota Shimazu <hornet.live.mf@gmail.com>
2018-03-07 01:11:29 +0900

    add copyright

--- ci/check.py
+++ ci/check.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env/python
+"""
+PINNA
+check.py
+
+Created by Shota Shimazu on 2018/03/07
+
+Copyright (c) 2018 Shota Shimazu All Rights Reserved.
+
+This software is released under the terms of restricted, see LICENSE for detail.
+https://hplab.work/pinna-music/pinna-music/blob/master/LICENSE
+"""
 
 import os, sys
 import subprocess
