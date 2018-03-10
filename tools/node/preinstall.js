a379a1686c2a7516073780dc0991e40f02ba80aa
Shota Shimazu <hornet.live.mf@gmail.com>
2018-03-11 00:52:49 +0900

    add npm install defender

---
+++ tools/node/preinstall.js
@@ -0,0 +1,17 @@
+/*
+PINNA
+preinstall-script.js
+
+Created by Shota Shimazu on 2018/03/11
+
+Copyright (c) 2018 Shota Shimazu All Rights Reserved.
+
+This software is released under the terms of restricted, see LICENSE for detail.
+https://hplab.work/pinna-music/pinna-music/blob/master/LICENSE
+*/
+
+if (process.env.npm_execpath.indexOf('yarn') === -1) {
+    console.error('You must use Yarn to install dependencies:');
+    console.error('  $ yarn install');
+    process.exit(1);
+}
