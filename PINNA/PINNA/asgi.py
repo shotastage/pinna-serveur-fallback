bc3908fef9476375ad4d5081b141671cd74cacfa
Shota Shimazu <hornet.live.mf@gmail.com>
2018-03-12 00:39:12 +0900

    add asgi

---
+++ PINNA/PINNA/asgi.py
@@ -0,0 +1,21 @@
+"""
+PINNA
+asgi.py
+
+Created by Shota Shimazu on 2018/03/12
+
+Copyright (c) 2018 Shota Shimazu All Rights Reserved.
+
+This software is released under the terms of restricted, see LICENSE for detail.
+https://hplab.work/pinna-music/pinna-music/blob/master/LICENSE
+"""
+
+
+import os
+import django
+from channels.routing import get_default_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "multichat.settings")
+
+django.setup()
+application = get_default_application()
