388fdbfb5f188f39f6e4e9b931df5645d4ba4e5e
Shota Shimazu <hornet.live.mf@gmail.com>
2018-03-11 20:02:23 +0900

    update models

--- PINNA/artillery/admin.py
+++ PINNA/artillery/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin
+"""
+PINNA
+admin.py
 
-# Register your models here.
+Created by Shota Shimazu on 2018/03/11
+
+Copyright (c) 2018 Shota Shimazu All Rights Reserved.
+
+This software is released under the terms of restricted, see LICENSE for detail.
+https://hplab.work/pinna-music/pinna-music/blob/master/LICENSE
+"""
+
+from django.contrib import admin
+from .models import PendingMail, SentMail
+
+admin.site.register(PendingMail)
+admin.site.register(SentMail)
--- PINNA/artillery/models.py
+++ PINNA/artillery/models.py
@@ -1,3 +1,34 @@
-from django.db import models
+"""
+PINNA
+models.py
 
-# Create your models here.
+Created by Shota Shimazu on 2018/03/11
+
+Copyright (c) 2018 Shota Shimazu All Rights Reserved.
+
+This software is released under the terms of restricted, see LICENSE for detail.
+https://hplab.work/pinna-music/pinna-music/blob/master/LICENSE
+"""
+
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from uuid import uuid4
+
+
+class PendingMail(models.Model):
+  mailid      = models.UUIDField(default = uuid4)
+  to_users    = ArrayField(models.CharField(max_length = 255), size=1000)
+  from_user   = models.CharField(max_length = 255)
+  title       = models.CharField(max_length = 255)
+  body        = models.TextField()
+  created_on  = models.DateTimeField()
+  is_sent     = models.BooleanField()
+
+
+class SentMail(models.Model):
+  mailid      = models.UUIDField(default = uuid4)
+  to_users    = ArrayField(models.CharField(max_length = 255), size=1000)
+  from_user   = models.CharField(max_length = 255)
+  title       = models.CharField(max_length = 255)
+  body        = models.TextField()
+  created_on  = models.DateTimeField()
