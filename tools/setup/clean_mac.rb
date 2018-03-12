2016cbfee6daac80fa909929e7d8edea695eba61
Shota Shimazu <hornet.live.mf@gmail.com>
2018-03-12 16:59:47 +0900

    add clean script

---
+++ tools/setup/clean_mac.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+
+"""
+PINNA
+uninstall_mac.rb
+
+Created by Shota Shimazu on 2018/03/12
+
+Copyright (c) 2018 Shota Shimazu All Rights Reserved.
+
+This software is released under the terms of restricted, see LICENSE for detail.
+https://hplab.work/pinna-music/pinna-music/blob/master/LICENSE
+"""
+
+require 'rbconfig'
+require 'tmpdir'
+require 'fileutils'
+
+
+class UninstallRecetPostgreApp
+    def initialize
+        remove()
+    end
+
+    def remove
+        puts "Removing Postgre.app ..."
+        Dir.chdir("/Applications/")
+        system("rm -rf Postgres.app")
+    end
+end
+
+class UninstallBrewPackages
+
+    def initialize
+        #puts "Uninstalling brew packages..."
+        #system("brew uninstall yarn node pyenv")
+    end
+
+end
+
+
+def main
+    UninstallRecetPostgreApp.new
+    UninstallBrewPackages.new
+end
+
+main
