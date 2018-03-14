b12f95f5725e69eba081218fe7dee7a8d981e245
Shota Shimazu <hornet.live.mf@gmail.com>
2018-03-15 00:43:07 +0900

    add new script

---
+++ tools/scripts/migration_to_new_gitlab.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+"""
+PINNA
+migration_to_new_gitlab.rb
+
+Created by Shota Shimazu on 2018/03/15
+
+Copyright (c) 2018 Shota Shimazu All Rights Reserved.
+
+This software is released under the terms of restricted, see LICENSE for detail.
+https://hplab.work/pinna-music/pinna-music/blob/master/LICENSE
+"""
+
+
+current_origin_url = `git config --get remote.origin.url`
+puts "Current origin: " + current_origin_url
+sepa = current_origin_url.split("@")
+new_origin_url = sepa[0] + "@" + "git." + sepa[1]
+puts "New origin: " + new_origin_url
+
+# Set new origin URL
+system("git remote rm origin")
+system("git remote add origin " + new_origin_url)
