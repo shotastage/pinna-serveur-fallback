ba7c9ab1536ac1ec165ac4f40277126a0b8a920c
Shota Shimazu <hornet.live.mf@gmail.com>
2018-03-19 15:43:29 +0900

    revise

---
+++ tools/setup/clean_linux.py
@@ -0,0 +1,14 @@
+"""
+PINNA
+clean_linux.py
+
+Created by Shota Shimazu on 2018/03/19
+
+Copyright (c) 2018 Shota Shimazu All Rights Reserved.
+
+This software is released under the terms of restricted, see LICENSE for detail.
+https://hplab.work/pinna-music/pinna-serveur/blob/master/LICENSE
+"""
+
+if __name__ == "__main__":
+    print("PINNA Devel Clean for Ubuntu")
