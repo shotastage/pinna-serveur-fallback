66c90ae08ee8cbdbe5370039407d3e24317cbce8
Shota Shimazu <hornet.live.mf@gmail.com>
2018-03-19 23:56:57 +0900

    revise

---
+++ mirage.config.py
@@ -0,0 +1,19 @@
+"""
+PINNA
+mirage.config.py
+
+Created by Shota Shimazu on 2018/03/19
+
+Copyright (c) 2018 Shota Shimazu All Rights Reserved.
+
+This software is released under the terms of restricted, see LICENSE for detail.
+https://hplab.work/pinna-music/pinna-serveur/blob/master/LICENSE
+"""
+
+import mirage
+
+
+MIRAGE_CONFIG_SCRIPT_VERSION = "0.0.1"
+
+def register_command():
+    return
---
+++ tools/setup/editor_setup_atom.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+PINNA
+editor_setup_atom.py
+
+Created by Shota Shimazu on 2018/03/19
+
+Copyright (c) 2018 Shota Shimazu All Rights Reserved.
+
+This software is released under the terms of restricted, see LICENSE for detail.
+https://hplab.work/pinna-music/pinna-serveur/blob/master/LICENSE
+"""
+
+if __name__ == "__main__":
+    pass
